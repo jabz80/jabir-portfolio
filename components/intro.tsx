45796939aae624630079d6713ebeea974de5cd7c
jabz80 <jabzhtc@gmail.com>
2023-12-15 19:50:39 +0000

    cv updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -69,7 +69,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
-          href="/jabir.ali.cv(final).pdf"
+          href="/jabir.ali.cv.pdf"
           download
           >
           Download CV <HiDownload 
